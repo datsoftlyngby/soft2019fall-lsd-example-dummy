f6f118716f9da3489fcf6570a44e59a4335c7b17
eguahlak <anders@kalhauge.dk>
2019-11-18 14:24:36 +0100

    cleanup

--- src/test/java/dk/cphbusiness/example/dummy/DummyManagerTest.java
+++ src/test/java/dk/cphbusiness/example/dummy/DummyManagerTest.java
@@ -1,9 +1,6 @@
 package dk.cphbusiness.example.dummy;
 
-import dk.cphbusiness.example.contract.Holder;
 import dk.cphbusiness.example.contract.PersonManagerTest;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;
