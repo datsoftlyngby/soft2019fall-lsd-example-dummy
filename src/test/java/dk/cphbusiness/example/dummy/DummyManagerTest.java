71f6f82ae21b47612eee56cbbee7a00a0ed89cf2
eguahlak <anders@kalhauge.dk>
2019-11-18 13:54:14 +0100

    tests

--- src/test/java/dk/cphbusiness/example/dummy/DummyManagerTest.java
+++ src/test/java/dk/cphbusiness/example/dummy/DummyManagerTest.java
@@ -1,10 +1,15 @@
 package dk.cphbusiness.example.dummy;
 
 import dk.cphbusiness.example.contract.Holder;
+import dk.cphbusiness.example.contract.PersonManagerTest;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
 
 
-@SelectClasses
+@RunWith(JUnitPlatform.class)
+@SelectClasses(PersonManagerTest.class)
 public class DummyManagerTest {
   
   @BeforeAll
@@ -14,5 +19,7 @@ public class DummyManagerTest {
   
   public DummyManagerTest() {
   }
+  
+  
   
 }
